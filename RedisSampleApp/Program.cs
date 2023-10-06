3ae0becd822cff80c56a6205c35df792423c0945
Vikash <tripvik@gmail.com>
2023-10-06 12:51:45 +0530

    Enabled swagger in prod

--- RedisSampleApp/Program.cs
+++ RedisSampleApp/Program.cs
@@ -22,12 +22,8 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
